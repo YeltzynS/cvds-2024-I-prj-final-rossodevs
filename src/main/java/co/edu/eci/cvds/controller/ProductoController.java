58deaad376d8201406bebb9a4bee1ba2d5b8a0d7
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-23 01:59:21 -0500

    Create ProductoController.java

---
+++ src/main/java/co/edu/eci/cvds/controller/ProductoController.java
@@ -0,0 +1,42 @@
+package co.edu.eci.cvds.controller;
+
+import co.edu.eci.cvds.model.Producto;
+import co.edu.eci.cvds.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public List<Producto> getAllProductos() {
+        return productoService.getAllProductos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> getProductoById(@PathVariable Long id) {
+        return productoService.getProductoById(id);
+    }
+
+    @PostMapping
+    public Producto createProducto(@RequestBody Producto producto) {
+        return productoService.createProducto(producto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> updateProducto(@PathVariable Long id, @RequestBody Producto producto) {
+        return productoService.updateProducto(id, producto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProducto(@PathVariable Long id) {
+        productoService.deleteProducto(id);
+    }
+}
