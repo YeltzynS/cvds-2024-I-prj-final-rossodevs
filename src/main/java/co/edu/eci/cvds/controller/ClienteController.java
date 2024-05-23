807a4544e4b4ff0c28e55faf8f125d90b864e297
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-23 01:58:54 -0500

    Create ClienteController.java

---
+++ src/main/java/co/edu/eci/cvds/controller/ClienteController.java
@@ -0,0 +1,42 @@
+package co.edu.eci.cvds.controller;
+
+import co.edu.eci.cvds.model.Cliente;
+import co.edu.eci.cvds.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public List<Cliente> getAllClientes() {
+        return clienteService.getAllClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
+        return clienteService.getClienteById(id);
+    }
+
+    @PostMapping
+    public Cliente createCliente(@RequestBody Cliente cliente) {
+        return clienteService.createCliente(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        return clienteService.updateCliente(id, cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCliente(@PathVariable Long id) {
+        clienteService.deleteCliente(id);
+    }
+}
