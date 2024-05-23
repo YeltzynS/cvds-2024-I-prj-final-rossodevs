f2264c54685ef5787242a0b1fcf749b4a40f09e3
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-23 01:57:18 -0500

    Create ServicioRepository.java

---
+++ src/main/java/co/edu/eci/cvds/repository/ServicioRepository.java
@@ -0,0 +1,7 @@
+package co.edu.eci.cvds.repository;
+
+import co.edu.eci.cvds.model.Servicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicioRepository extends JpaRepository<Servicio, Long> {
+}
