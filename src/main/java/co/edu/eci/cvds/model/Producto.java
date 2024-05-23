a23779deb8cf153b2b3eb2f1f1653df5dc4b29f8
YeltzynS <111905803+YeltzynS@users.noreply.github.com>
2024-05-23 01:51:13 -0500

    Create Producto.java

---
+++ src/main/java/co/edu/eci/cvds/model/Producto.java
@@ -0,0 +1,92 @@
+package co.edu.eci.cvds.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "PRODUCTO")
+public class Producto {
+    @Id
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NOMBRE")
+    private String nombre;
+
+    @Column(name = "VENTAS")
+    private int ventas;
+
+    public Producto() {
+    }
+
+    public Producto(Long id, String nombre, int ventas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ventas = ventas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(int ventas) {
+        this.ventas = ventas;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + ventas;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Producto other = (Producto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (ventas != other.ventas)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto [id=" + id + ", nombre=" + nombre + ", ventas=" + ventas + "]";
+    }
+}
